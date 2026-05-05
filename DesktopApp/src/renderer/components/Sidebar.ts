d79900aa9652638e4b33e9c66468ff5809352cd2
M B A R K <mbark1236@gmail.com>
2026-05-05 14:02:04 +0100

    ui(Desktop): Add the Official Logo

--- DesktopApp/src/renderer/components/Sidebar.ts
+++ DesktopApp/src/renderer/components/Sidebar.ts
@@ -68,15 +68,7 @@ export class Sidebar extends BaseComponent {
         <div class="sidebar-header">
           <div class="sidebar-logo">
             <div class="sidebar-logo-icon">
-              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"
-                fill="none" stroke="currentColor" stroke-width="2"
-                stroke-linecap="round" stroke-linejoin="round">
-                <circle cx="12" cy="12" r="2"/>
-                <path d="M16.24 7.76a6 6 0 0 1 0 8.49"/>
-                <path d="M7.76 16.24a6 6 0 0 1 0-8.49"/>
-                <path d="M19.07 4.93a10 10 0 0 1 0 14.14"/>
-                <path d="M4.93 19.07a10 10 0 0 1 0-14.14"/>
-              </svg>
+              <img src="./assets/logo.png" alt="Aether Radio" class="sidebar-logo-img">
             </div>
             <div class="sidebar-logo-text">
               <span class="sidebar-logo-name">Aether Radio</span>
--- DesktopApp/src/renderer/views/SettingsView.ts
+++ DesktopApp/src/renderer/views/SettingsView.ts
@@ -118,15 +118,7 @@ export class SettingsView extends BaseComponent {
           </div>
           <div class="stg-about">
             <div class="stg-about-logo">
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                fill="none" stroke="currentColor" stroke-width="1.75"
-                stroke-linecap="round" stroke-linejoin="round">
-                <circle cx="12" cy="12" r="2"/>
-                <path d="M16.24 7.76a6 6 0 0 1 0 8.49"/>
-                <path d="M7.76 16.24a6 6 0 0 1 0-8.49"/>
-                <path d="M19.07 4.93a10 10 0 0 1 0 14.14"/>
-                <path d="M4.93 19.07a10 10 0 0 1 0-14.14"/>
-              </svg>
+              <img src="./assets/logo.png" alt="Aether Radio" class="stg-about-logo-img">
             </div>
             <div>
               <div class="stg-about-name">Aether Radio</div>
