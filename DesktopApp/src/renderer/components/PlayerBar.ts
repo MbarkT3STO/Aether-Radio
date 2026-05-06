3f61766c27ab9b9a4d3e76d835ad68deedc4a5e4
M B A R K <mbark1236@gmail.com>
2026-05-06 09:52:36 +0100

    style(Desktop): Update the Music Recognition Button Icon

--- DesktopApp/src/renderer/components/PlayerBar.ts
+++ DesktopApp/src/renderer/components/PlayerBar.ts
@@ -1171,9 +1171,11 @@ export class PlayerBar extends BaseComponent {
     return `<svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24"
       fill="none" stroke="currentColor" stroke-width="2"
       stroke-linecap="round" stroke-linejoin="round">
-      <path d="M9 18V5l12-2v13"/>
-      <circle cx="6" cy="18" r="3"/>
-      <circle cx="18" cy="16" r="3"/>
+      <path d="M9 2v20"/>
+      <path d="M5 6v12"/>
+      <path d="M13 4v16"/>
+      <path d="M17 7v10"/>
+      <path d="M21 10v4"/>
     </svg>`
   }
 
